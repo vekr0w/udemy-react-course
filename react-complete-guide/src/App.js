639fc32798e4e8259272c36af10eae9e7a92c9ed
vekr0w <vekr0w@gmail.com>
2021-02-23 15:21:24 +0200

    lect40 props,state,dynamic values.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p>This is really working!</p>
-        <Person />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}> My hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a react App'))
