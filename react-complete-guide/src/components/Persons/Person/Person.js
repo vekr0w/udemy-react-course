72c1fd3a2d248907cfdbb84ebc24aed46fecb498
Petar_Grigorov <vekr0w@gmail.com>
2021-03-31 14:31:41 +0300

    Lec103: Hoc/Aux use

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import styles from './Person.module.css';
+import Aux from '../../../hoc/Auxilliary/Auxilliary'
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={styles.Person}>
-        <p onClick={this.props.click}> I'm a {this.props.name} and I am {this.props.age} years old!</p>
-        <p> {this.props.children}</p>
+      <Aux>
+        <p onClick={this.props.click}>
+          I'm a {this.props.name} and I am {this.props.age} years old!
+           </p>
+        <p > {this.props.children}</p>
         <input type="text"
           onChange={this.props.changed}
-          value={this.props.name} />
-      </div>
-    )
+          value={this.props.name}
+        />
+      </Aux>);
   }
 };
 
