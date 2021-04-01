0db1ecf0e665f28aca00518d982f7a2aa6c502f0
Petar_Grigorov <vekr0w@gmail.com>
2021-04-01 15:01:20 +0300

    Lec125: Burger Builder

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div>
+import React, { Component } from 'react';
+import Layout from './components/containers/Layout/Layout';
+import BurgerBuilder from './components/containers/BurgerBuilder/BurgerBuilder';
 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
