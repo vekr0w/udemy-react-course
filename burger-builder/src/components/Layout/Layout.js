e23f9df8da7ebbb17ac0830be045713052363334
Petar_Grigorov <vekr0w@gmail.com>
2021-04-05 11:50:28 +0300

    Lec126: Burger ingredient added

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../../hoc/Auxilliary';
+import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.module.css';
 
 const layout = (props) => (
