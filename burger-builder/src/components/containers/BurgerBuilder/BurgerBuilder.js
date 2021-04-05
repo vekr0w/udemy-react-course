9a768f67336d1708f2bbafdda05b6b6220db4245
Petar_Grigorov <vekr0w@gmail.com>
2021-04-05 14:10:52 +0300

    Lec130: Adding Dynamic Ingredients

--- burger-builder/src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,21 @@ import Burger from '../../Burger/Burger';
 
 class BurgerBuilder extends Component {
 
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+
+
 
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build controls</div>
       </Aux>
     );
