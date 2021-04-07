bc324866e1e150f1e4a5eaa521a9245a2222bd90
Petar_Grigorov <vekr0w@gmail.com>
2021-04-07 13:56:48 +0300

    Lec141: Added buttons to order summary

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Auxilliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -18,6 +19,12 @@ const orderSummary = (props) => {
         {ingredientSummary}
       </ul>
       <p>Continue to Checkout?</p>
+      <Button
+        btnType="Danger"
+        clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button
+        btnType="Success"
+        clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
 }
